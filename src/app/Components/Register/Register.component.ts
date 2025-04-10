cf8b16d654b6fb67b3faedca4a92598de8b85c9a
MennaHassan26 <mennahassan2010st@gmail.com>
2025-04-10 05:56:16 +0200

    Added Home,Login,and register

--- src/app/Components/Register/Register.component.ts
+++ src/app/Components/Register/Register.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate , state } from '@angular/animations';
 
 @Component({
   selector: 'app-Register',
   templateUrl: './Register.component.html',
-  styleUrls: ['./Register.component.css']
+  styleUrls: ['./Register.component.css'],
+ animations: [
+    trigger('imageSlide', [
+      state('login', style({ transform: 'translateX(0%)' })),
+      state('register', style({ transform: 'translateX(100%)' })),
+      transition('login <=> register', animate('800ms ease-in-out')),
+    ]),
+    trigger('formFade', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate('500ms 300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+      transition(':leave', [
+        animate('200ms ease-in', style({ opacity: 0, transform: 'translateY(20px)' }))
+      ]),
+    ]),
+  ],
 })
 export class RegisterComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+  isRegistered: boolean = true;
+
+  toggleForm() {
+    this.isRegistered = !this.isRegistered;
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from './Components/Home/Home.component';
+
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent }, 
+
   { path: 'shopping-cart', loadChildren: () => import('./shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule)  },
   { path: 'wish-list',  loadChildren: () => import('./wish-list/wish-list.module').then(m => m.WishListModule) },
   { path: 'Profile', loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule) }  
