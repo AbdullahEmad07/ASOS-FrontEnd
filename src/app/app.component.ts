cc6017a1baa9233349babfc792647d1b329f697c
Alaaelsayed0110 <Alaaelsayed0110@gmail.com>
2025-04-11 23:30:46 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { MenComponent } from "./Components/Men/Men.component";
 import { WomenComponent } from "./Components/Women/Women.component";
 import { ProfileComponent } from "./profile/Profile/Profile.component";
 import { HomeComponent } from "./Components/Home/Home.component";
+import { LoginComponent } from "./Components/login/login.component";
 
 @Component({
   selector: 'app-root',
