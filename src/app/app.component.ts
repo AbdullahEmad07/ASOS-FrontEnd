841113d6c313b65bd2cd19e6035ab7f83892b3ee
Alaaelsayed0110 <Alaaelsayed0110@gmail.com>
2025-04-14 14:46:08 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { WomenComponent } from "./Components/Women/Women.component";
 import { ProfileComponent } from "./profile/Profile/Profile.component";
 import { HomeComponent } from "./Components/Home/Home.component";
 import { LoginComponent } from "./Components/login/login.component";
+import { SingleProductComponent } from "./Components/single-product/single-product.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [SingleProductComponent],
 })
 export class AppComponent {
   title = 'asos';
