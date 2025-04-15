1703b9c4a9130b1b904a28d3c467e8583ba34fb2
mahmoud adel <M777hmoued@gmail.com>
2025-04-15 23:31:24 +0200

    Add Shoping Cart Service

--- src/app/Services/shopping-cart.service.ts
+++ src/app/Services/shopping-cart.service.ts
@@ -1,13 +1,49 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingCartService {
+  private baseUrl = environment.apiUrl;
 
-  private baseUrl = environment.apiUrl; // Use the apiUrl from environment
-  
-  constructor() { }
-  
+  constructor(private _Http: HttpClient) { }
+
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+    });
+  }
+
+  getCartProducts(): Observable<any> {
+    return this._Http.get(`${this.baseUrl}/Cart/products`, {
+      headers: this.getHeaders()
+    }).pipe(
+      tap({
+        next: (response) => console.log('Cart Response:', response),
+        error: (error) => console.error('Cart Error:', error)
+      })
+    );
+  }
+
+  addToCart(productId: string): Observable<any> {
+    return this._Http.post(`${this.baseUrl}/Cart/products/${productId}`, {}, {
+      headers: this.getHeaders()
+    });
+  }
+
+  removeFromCart(productId: string): Observable<any> {
+    return this._Http.delete(`${this.baseUrl}/Cart/products/${productId}`, {
+      headers: this.getHeaders()
+    });
+  }
+
+  updateQuantity(productId: string, quantity: number): Observable<any> {
+    return this._Http.put(`${this.baseUrl}/Cart/products/${productId}/${quantity}`, {}, {
+      headers: this.getHeaders()
+    });
+  }
 }
